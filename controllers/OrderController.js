c8bb4eff47cbe14d63e4d257778e4337d5a4e228
Manish Shaw <shawmanish2580@gmail.com>
2024-06-19 17:45:46 +0530

    added order functions and payment system

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -1,20 +1,45 @@
+import Cart from '../model/CartModel.js';
 import Order from '../model/Order.js';
+import UserInfo from '../model/UserInfo.js';
+import Stripe from "stripe";
 
 const svaeOrderDetails = async () => {
     
     try {
-    const { user, products, total, totalItems, recepientDetails } = req.body;
-    console.log('req.body : ',req.body)
-    const newOrder = new Order({
-      user,
-      products,
-      totaAmount,
-      totalItems,
-      recepientDetails
-    });
+    const {userId} = req.user;
 
-    const savedOrder = await newOrder.save();
-    res.status(200).json(savedOrder);
+    const { user, products, totalAmount, totalItems, recepientDetails } = req.body;
+    console.log('req.body : ',req.body)
+
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+        console.log('stripe : ',stripe)
+
+    const userInfo = await UserInfo.findOne({ userId });
+    console.log('userInfo : ',userInfo);
+
+    const CartDetails = await Cart.findOne({ userId }).populate('products');
+    console.log('CartDetails : ',CartDetails);
+
+    const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: CartDetails?.products,
+    mode: "payment",
+    success_url: "http://localhost:3000/success",
+    cancel_url: "http://localhost:3000/cancel",
+    });
+    
+    console.log('session : ',session)
+
+    // const newOrder = new Order({
+    //   user,
+    //   products,
+    //   totaAmount,
+    //   totalItems,
+    //   recepientDetails
+    // });
+
+    // const savedOrder = await newOrder.save();
+    res.status(200).json({success:true,Data:{id:session?.id}});
 
   } catch (error) {
     console.error('Failed to save order:', error);
