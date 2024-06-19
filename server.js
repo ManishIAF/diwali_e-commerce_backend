a0dcf05f6fbb34236d156ff2bba647c01a215715
Manish Shaw <shawmanish2580@gmail.com>
2024-06-19 16:57:36 +0530

    added order functions and payment system

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ const upload = multer({storage : multer.memoryStorage()});
 //-------------------------------------------importing Routes----------------------------------------------
 
 // import registerRoute from './routes/registerRoute.js'
-import loginRoute from './routes/loginRoute.js'
+// import loginRoute from './routes/loginRoute.js'
 import wishListRoute from './routes/watchlist.js'
 import cartRoute from './routes/cartRoute.js'
 import clothingRouter from './routes/clothingRoute.js'
