f754eaa7bde9ad97fe6cad0752e59df6d51f02c5
Manish Shaw <shawmanish2580@gmail.com>
2024-06-19 17:33:15 +0530

    added order functions and payment system

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -1,9 +1,10 @@
 import Order from '../model/Order.js';
 
 const svaeOrderDetails = async () => {
+    
+    try {
     const { user, products, total, totalItems, recepientDetails } = req.body;
-
-  try {
+    console.log('req.body : ',req.body)
     const newOrder = new Order({
       user,
       products,
@@ -14,7 +15,7 @@ const svaeOrderDetails = async () => {
 
     const savedOrder = await newOrder.save();
     res.status(200).json(savedOrder);
-    
+
   } catch (error) {
     console.error('Failed to save order:', error);
     res.status(500).json({ message: 'Failed to save order', error });
